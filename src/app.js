d37a995e2a94212300dda3f0347aea3a2ba6e2db
Saiful Saif <mdsaifulislam02137@gmail.com>
2026-03-01 10:56:26 +0600

    setup project

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+
+
+module.exports = app; 
